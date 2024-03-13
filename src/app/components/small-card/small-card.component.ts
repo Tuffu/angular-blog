c5b826aa3a77898d329d1697a70188049547a1f3
Tuffu <lucaspaes55@gmail.com>
2024-03-13 16:48:27 -0300

    insert id in html

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,8 @@ export class SmallCardComponent implements OnInit {
   photoCover:string=""
   @Input()
   cardTitle:string="Saiu uma nova versão do angular"
+  @Input()
+  Id:string="0"
 
   constructor() { }
 
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -9,9 +10,15 @@ export class ContentComponent implements OnInit {
   photoCover:string="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"
   contentTitle:string="Meu artigo"
   contentDescription:string="minha descrição"
-  constructor() { }
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      console.log(value.get("id"))
+    )
   }
 
 }
